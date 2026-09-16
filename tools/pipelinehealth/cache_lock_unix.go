618768e38fd052001d3f43e1d89b02c03a46b2fd
ivanarama <83814559+ivanarama@users.noreply.github.com>
2026-09-17 02:20:41 +0300

    fix(pipeline): сократить расход GitHub API
    Перевести pipelinehealth на полный пакетный GraphQL-снимок и оставить условный REST ETag-кэш как явный fail-closed резерв.
    
    Generated-with: Codex

---
+++ tools/pipelinehealth/cache_lock_unix.go
@@ -0,0 +1,52 @@
+//go:build !windows
+
+package main
+
+import (
+	"context"
+	"errors"
+	"os"
+	"time"
+
+	"golang.org/x/sys/unix"
+)
+
+type cacheFileLock struct {
+	file *os.File
+}
+
+func acquireCacheFileLock(ctx context.Context, path string) (*cacheFileLock, error) {
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR, 0o600)
+	if err != nil {
+		return nil, err
+	}
+	if err := file.Chmod(0o600); err != nil {
+		_ = file.Close()
+		return nil, err
+	}
+	for {
+		err = unix.Flock(int(file.Fd()), unix.LOCK_EX|unix.LOCK_NB)
+		if err == nil {
+			return &cacheFileLock{file: file}, nil
+		}
+		if !errors.Is(err, unix.EWOULDBLOCK) && !errors.Is(err, unix.EAGAIN) {
+			_ = file.Close()
+			return nil, err
+		}
+		select {
+		case <-ctx.Done():
+			_ = file.Close()
+			return nil, ctx.Err()
+		case <-time.After(20 * time.Millisecond):
+		}
+	}
+}
+
+func (lock *cacheFileLock) Close() error {
+	unlockErr := unix.Flock(int(lock.file.Fd()), unix.LOCK_UN)
+	closeErr := lock.file.Close()
+	if unlockErr != nil {
+		return unlockErr
+	}
+	return closeErr
+}
